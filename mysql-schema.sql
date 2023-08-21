42f945d77d0896557518eddf4707ee1e1d334f0f
Zasoba <vladyslav.v.zasoba@mercedes-benz.com>
2023-08-21 21:55:59 +0200

    Remove pipeline

---
+++ mysql-schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE board (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE task_list (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(64) NOT NULL,
+    board_id INTEGER NOT NULL,
+    FOREIGN KEY (board_id) REFERENCES board(id)
+)
+
+CREATE TABLE task (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(64) NOT NULL,
+    description VARCHAR(255),
+    task_list_id INTEGER NOT NULL,
+    FOREIGN KEY (task_list_id) REFERENCES task_list(id)
+)
